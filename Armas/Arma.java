90cb700f30c2f5350c86a4bea705edcfc5f662d9
ovinicius71 <viniciusoliveiralima72@gmail.com>
2024-05-27 13:24:51 -0400

    att classe arma

--- Armas/Arma.java
+++ Armas/Arma.java
@@ -1,18 +1,18 @@
 package Armas;
-import Personagens.Personagem;
 
-public abstract class Arma extends Personagem {
+public abstract class Arma {
     
     private String nome;
     private double modDano;
 
     public Arma (String nome, double modificador){
-
+        this.nome = nome;
+        this.modDano = modificador;
     }
     public String getNome(){
-
+        return nome;
     }
     public double getModificadorDano(){
-
+        return modDano;
     }
 }
